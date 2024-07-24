c5bbd4d72cf40b329d35735ebc7e028f0302917e
smitsu32 <166088078+smitsu32@users.noreply.github.com>
2024-07-24 18:48:56 +0900

    230c

---
+++ Visual_Studio_py/legacyc/abc230c.py
@@ -0,0 +1,21 @@
+n,a,b=map(int,input().split())
+p,q,r,s=map(int,input().split())
+
+# (a,b)を通り、X状の位置を#にする
+# Input1: (3,2)から斜め四方向に黒を延ばす
+# p<=i<=q+1, r<=j<=s+1のグリッドを出力
+
+for i in range(p,q+1):
+    ans=['.']*(s-r+1)
+    for j in range(r,s+1):
+        if i==j+(a-b) or i==-j+(a+b):   # ij平面で直線の式(j=(+-ai)+b)をたてる
+            ans[j-r]='#'
+    print(''.join(ans))
+
+#   # . . . # j
+#   . # . # .
+#   . . # . # b
+#   . # . # .
+#   # . . . #
+#   i   a
+# (a,b)=(3,3)
