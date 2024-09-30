a19000f766ff092cc8ed12152f0627b6f87b0f41
smitsu32 <166088078+smitsu32@users.noreply.github.com>
2024-09-30 16:56:49 +0900

    add pypyjit in 373d

--- Visual_Studio_py/legacyd/abc373d.DBFS.py
+++ Visual_Studio_py/legacyd/abc373d.DBFS.py
@@ -1,6 +1,8 @@
 from collections import deque
 import sys
 sys.setrecursionlimit(10**6)
+# import pypyjit
+# pypyjit.set_param('max_unroll_recursion=-1')
 
 n,m=map(int,input().split())
 INF=float('inf')
