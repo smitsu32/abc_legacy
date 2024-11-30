6a1e40ab82ea05ff5814bee75015f20e4c4fa579
smitsu32 <166088078+smitsu32@users.noreply.github.com>
2024-12-01 02:33:40 +0900

    change addpath

--- MATLAB_haiden/+mf/addpath.m
+++ MATLAB_haiden/+mf/addpath.m
@@ -1,7 +1,7 @@
 function  addpath()
 %ADDPAH この関数の概要をここに記述
 %   詳細説明をここに記述
-nowDir = genpath(pwd);
-addpath(nowDir);
-fprintf('addpath: %s\n', pwd);
+[currentFolder, ~, ~] = fileparts(mfilename('fullpath');
+addpath(currentFolder);
+fprintf('%s をパスに追加しました\n', pwd);
 end
