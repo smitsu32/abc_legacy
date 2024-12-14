92284ae938c6b2809b528eb1264ebc5174ef91a1
smitsu32 <166088078+smitsu32@users.noreply.github.com>
2024-12-14 22:54:56 +0900

    revise 384c

--- Visual_Studio_py/legacyc/abc384c.py
+++ Visual_Studio_py/legacyc/abc384c.py
@@ -1,27 +1,33 @@
-n,s=map(int,input().split())
+from itertools import combinations
+from bisect import bisect_right,bisect_left
+
 a=list(map(int,input().split()))
-b=sum(a)
+al='ABCDE'
+t=[]
+for r in range(1,len(al)+1):
+    for i in combinations(al,r):
+        t.append(''.join(i))
+t.sort()
+ans=[]
+ansind=[]
+for i in t:
+    n=len(i)
+    ansi=0
+    for j in range(n):
+        if i[j] in al:
+            temp=ord(i[j])-65
+            ansi+=a[temp]
+    
+    if len(ans)==0:
+        ans.append(ansi)
+        ansind.append(i)
+        continue
+    
+    pos=bisect_left(ans,ansi)
+    ansind.insert(pos,i)
+    ans.insert(pos,ansi)
 
-s%=b
-if s==0:
-    print('Yes')
-    exit()
-
-a+=a
-S=[a[0]]*2*n
-for i in range(1,2*n):
-    S[i]=S[i-1]+a[i]
-
-i,j=0,0
-while i<n and j<2*n:
-    diff=S[j]-S[i]
-    if diff==s:
-        print('Yes')
-        exit()
-    elif diff>s:
-        i+=1
-        j=max(j,i)
-    else:
-        j+=1
-
-print('No')
+ansind=ansind[::-1]
+ans=ans[::-1]
+for i in range(len(ansind)):
+    print(ansind[i])
